6dbbdc4bf31082cb65c00bac4d903b0f5c6895f7
Dm1stry <BattleshipWriter@yandex.ru>
2023-09-11 02:39:15 +0300

    TikhonovProcessWidget first version (just view)

--- tikhonovprocesswidget.cpp
+++ tikhonovprocesswidget.cpp
@@ -1,7 +1,30 @@
 #include "tikhonovprocesswidget.h"
 
 TikhonovProcessWidget::TikhonovProcessWidget(QWidget * parent)
-  : BaseProcessWidget(parent)
+  : BaseProcessWidget("Тихонов", parent)
 {
-    labels_ << *(new QLabel("T<sub>2, min</sub>")) << *(new QLabel("T<sub>2, max</sub>"));
+    this->parameters_.insert("T<sub>2, min</sub>", new QLineEdit);
+    this->parameters_.insert("T<sub>2, max</sub>", new QLineEdit);
+
+    QVBoxLayout * widget_layout = new QVBoxLayout;
+    QFormLayout * parameters_layout = new QFormLayout;
+
+    for(auto parameter = parameters_.keyBegin(); parameter != parameters_.keyEnd(); ++parameter)
+    {
+        parameters_layout->addRow(*parameter, parameters_[*parameter]);
+    }
+    widget_layout->addLayout(parameters_layout);
+    widget_layout->addLayout(this->buttons_layout_);
+
+    this->setLayout(widget_layout);
+}
+
+/*virtual*/ void TikhonovProcessWidget::clearParams()
+{
+
+}
+
+/*virtual*/ void TikhonovProcessWidget::Process()
+{
+
 }
--- tikhonovprocesswidget.h
+++ tikhonovprocesswidget.h
@@ -1,6 +1,9 @@
 #ifndef TIKHONOVPROCESSWIDGET_H
 #define TIKHONOVPROCESSWIDGET_H
 
+#include <QFormLayout>
+#include <QVBoxLayout>
+
 #include "baseprocesswidget.h"
 
 class TikhonovProcessWidget : public BaseProcessWidget
@@ -8,6 +11,9 @@ class TikhonovProcessWidget : public BaseProcessWidget
 Q_OBJECT
 public:
     TikhonovProcessWidget(QWidget * parent = nullptr);
+public slots:
+    virtual void clearParams();
+    virtual void Process();
 };
 
 #endif
