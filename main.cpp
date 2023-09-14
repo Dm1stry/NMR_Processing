3d2d0874a7a11bbfc44be5592ea0895a0d4d204c
Dm1stry <BattleshipWriter@yandex.ru>
2023-09-14 17:11:18 +0300

    Structure of project slightly changed

--- main.cpp
+++ main.cpp
@@ -1,5 +1,5 @@
-#include "mainwindow.h"
-#include "filesystemwidget.h"
+#include "widgets/mainwindow.h"
+#include "widgets/filesystemwidget.h"
 #include <QApplication>
 
 int main(int argc, char *argv[])
@@ -9,25 +9,3 @@ int main(int argc, char *argv[])
     w->show();
     return a.exec();
 }
-
-/*
-#include <QApplication>
-#include "dependencies/QCustomPlot/qcustomplot.h"
-
-int main(int argc, char *argv[])
-{
-    QApplication app(argc, argv);
-
-    // Создание экземпляра QCustomPlot
-    QCustomPlot *customPlot = new QCustomPlot;
-
-    // Добавление QCustomPlot на форму
-    QWidget *widget = new QWidget;
-    QVBoxLayout *layout = new QVBoxLayout;
-    layout->addWidget(customPlot);
-    widget->setLayout(layout);
-
-    widget->show();
-
-    return app.exec();
-}*/
