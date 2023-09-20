b55cc1072f12620debc82f2b609193a11100d1d4
Dm1stry <BattleshipWriter@yandex.ru>
2023-09-20 10:12:08 +0300

    Added math function for trapezoid

--- mathfunctions.h
+++ mathfunctions.h
@@ -2,8 +2,23 @@
 #define MATHFUNCTIONS_H
 #include <vector>
 
+inline double trapz_intergal(std::vector<double> x, std::vector<double> y);
 
-double trapz_intergal(std::vector<double> x, std::vector<double> y);
+template<class RandomIt> inline double trapz_intergal(RandomIt x_begin, RandomIt x_end, RandomIt y_begin, RandomIt y_end)
+{
+    if(x_begin == x_end || x_end - x_begin != y_end - y_begin)
+    {
+        return 0;
+    }
+    double sum = (*y_begin) * (*(x_begin + 1) - (*x_begin)) / 2;
+    for(; x_begin < x_end; x_begin++, y_begin++)
+    {
+        sum += (*y_begin) * (*(x_begin + 1) - (*x_begin));
+    }
+    sum += *(y_end - 1) * (*(x_end - 1) - *(x_end - 2));
+
+    return sum;
+}
 
 inline std::vector<double> logspace(double start, double stop, size_t number, double base = 10.0, bool endpoint = true);
 
