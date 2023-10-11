e8ef6a9c64c484adcf0d90da8e1b60993f304b55
Dm1stry <BattleshipWriter@yandex.ru>
2023-10-11 10:55:26 +0300

    SequentalProcessWidget Added and connected

--- widgets/sequentalprocesswidget.cpp
+++ widgets/sequentalprocesswidget.cpp
@@ -4,5 +4,16 @@ SequentalProcessWidget::SequentalProcessWidget(QWidget * parent)
   : BaseProcessWidget("Посл. аппроксимация", parent)
 {
     BaseProcessor * processor = new SequentalProcessor();
-    
+    this->setProcessorInSeparateThread(processor);
+
+	this->addParameter("N_max", "N<sub>max</sub>", 5);
+
+	QVBoxLayout * widget_layout = new QVBoxLayout;
+	QGridLayout * parameters_layout = this->getParametersLayout();
+	QGridLayout * buttons_layout = this->getButtonsLayout();
+
+	widget_layout->addLayout(parameters_layout);
+    widget_layout->addLayout(buttons_layout);
+
+    this->setLayout(widget_layout);
 }
